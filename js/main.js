7b9865079243240a57ee744a4d67b2c58238572f
Anton Voltchok <antonvoltchok1@gmail.com>
2018-04-30 11:56:06 -0400

    put password back on

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+jQuery(function ($) {
+
+  var pass_entered;
+  var password="SR_BBB2018";
+
+  while (pass_entered!=password) {
+    pass_entered=prompt("Please enter the password:",'')
+  }
+
+  self.close();
+  document.body.style.display = "block";
+
+});
